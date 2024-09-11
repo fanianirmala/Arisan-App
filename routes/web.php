cd2b89fe875dc5903e5ff9f7feb80a0c619a9768
Fania-Nirmala <nirmalafania62@gmail.com>
2024-09-11 10:08:02 +0700

    route-sidebar

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 })->name('login');
-
 Route::get('/event-arisan-baru', function () {
     return view('event-arisan-baru');
 })->name('event-arisan-baru');
 Route::get('/register', function () {
     return view('register');
 })->name('register');
+Route::get('/sidebar', function () {
+    return view('template.sidebar');
+})->name('sidebar');
