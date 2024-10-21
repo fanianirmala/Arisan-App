32c672b376d080ac0f2e3c0c24c54ef9c5aca8c2
wanda <wandahalimatu91@gmail.com>
2024-10-21 07:45:13 +0700

    daftar-user

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('splash-screen');
 });
+Route::get('/table', function () {
+    return view('table');
+});
+Route::get('/detail-deposit', function () {
+    return view('detail-deposit');
+});
+
+Route::get('/daftar-user', function() {
+    return view('daftar-user');
+});
+Route::get('/detail-user1', function() {
+    return view('detail-user1');
+});
 Route::get('/sidebar', function () {
     return view('template.sidebar');
 });
+
 Route::get('/test', function () {
     return view('test');
 });
