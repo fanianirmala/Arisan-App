4d87cb6b3e1527a5238729edcec78d6990a1884c
Fania-Nirmala <nirmalafania62@gmail.com>
2024-10-23 13:50:12 +0700

    push

--- resources/views/detailUser-dataPersonal.blade.php
+++ resources/views/detailUser-dataPersonal.blade.php
@@ -157,41 +157,28 @@
                         <th>Jenis</th>
                         <th>Nominal</th>
                         <th>Bukti Transaksi</th>
+                        <th>Tanggal Transaksi</th>
                         <th>Deskripsi</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td>61</td>
-                        <td>2011-04-25</td>
-                        <td>$320,800</td>
+                        <td>Transfer Bank Lain</td>
+                        <td>Bank Mandiri Johnatan : 1234567</td>
+                        <td>Transfer Bank Lain</td>
+                        <td>Rp. 10.000.000</td>
+                        <td> <a href="#">Lihat Bukti</a> </td>
+                        <td>28 Agustus 2026</td>
+                        <td>Uang Saluran</td>
                     </tr>
                     <tr>
-                        <td>Garrett Winters</td>
-                        <td>Accountant</td>
-                        <td>Tokyo</td>
-                        <td>63</td>
-                        <td>2011-07-25</td>
-                        <td>$170,750</td>
-                    </tr>
-                    <tr>
-                        <td>Ashton Cox</td>
-                        <td>Junior Technical Author</td>
-                        <td>San Francisco</td>
-                        <td>66</td>
-                        <td>2009-01-12</td>
-                        <td>$86,000</td>
-                    </tr>
-                    <tr>
-                        <td>Cedric Kelly</td>
-                        <td>Senior Javascript Developer</td>
-                        <td>Edinburgh</td>
-                        <td>22</td>
-                        <td>2012-03-29</td>
-                        <td>$433,060</td>
+                        <td>Transfer E-Wallet</td>
+                        <td>OVO Johnatan : 1234567</td>
+                        <td>Transfer Bank Lain</td>
+                        <td>Rp. 10.000.000</td>
+                        <td> <a href="#">Lihat Bukti</a> </td>
+                        <td>28 Agustus 2026</td>
+                        <td>Uang Saluran</td>
                     </tr>
                 </tbody>
                 <tfoot>
--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -9,6 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.css">
     <link rel="stylesheet" href="css/sidebar.css">
     <title>Sidebar</title>
 </head>
